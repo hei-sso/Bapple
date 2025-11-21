03ac6439436bc2fb315f96f892c966e8b9b16a79
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-21 16:58:49 +0900

    Update user_allergy.sql

--- user_allergy.sql
+++ user_allergy.sql
@@ -1,8 +1,17 @@
 CREATE TABLE user_allergy (
-    user_id BIGINT NOT NULL,
-    allergy_id INT NOT NULL,
-    
+    user_id    BIGINT NOT NULL,
+    allergy_id VARCHAR(50) NOT NULL,
+
     PRIMARY KEY (user_id, allergy_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (allergy_id) REFERENCES allergy(allergy_id)
-);
+
+    CONSTRAINT fk_user_allergy_user
+        FOREIGN KEY (user_id)
+        REFERENCES user(user_id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_user_allergy_allergy
+        FOREIGN KEY (allergy_id)
+        REFERENCES allergy(allergy_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET = utf8mb4
+COLLATE = utf8mb4_general_ci;
