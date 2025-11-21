306b51ea75ca49cdd69dc5c92d06d11054312f80
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-21 16:57:05 +0900

    Update allergy.sql

--- allergy.sql
+++ allergy.sql
@@ -1,4 +1,7 @@
 CREATE TABLE allergy (
-    allergy_id VARCHAR(50) PRIMARY KEY, 
-    allergy_name VARCHAR(50) NOT NULL UNIQUE
-);
+    allergy_id  VARCHAR(50) PRIMARY KEY,      -- 문자 PK
+    allergy_name VARCHAR(50) NOT NULL UNIQUE  -- 알러지명
+)
+ENGINE=InnoDB
+DEFAULT CHARSET = utf8mb4
+COLLATE = utf8mb4_general_ci;
