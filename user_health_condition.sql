cc5d24057f537161391f7015d90a01547cfe97fc
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-27 15:35:20 +0900

    Update user_health_condition.sql

--- user_health_condition.sql
+++ user_health_condition.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_health_condition (
     user_id BIGINT NOT NULL,
-    health_condition_id INT NOT NULL,
+    health_condition_id VARCHAR(50) NOT NULL,
     
     PRIMARY KEY (user_id, health_condition_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
