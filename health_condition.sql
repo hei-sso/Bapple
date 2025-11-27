a57ccb22d40310545d21960149d5f0bce88d6a3a
Gwanpyeong <155734998+Gwanpyeong@users.noreply.github.com>
2025-11-27 15:35:02 +0900

    Update health_condition.sql

--- health_condition.sql
+++ health_condition.sql
@@ -1,5 +1,5 @@
 CREATE TABLE health_condition (
-    health_condition_id INT AUTO_INCREMENT PRIMARY KEY,
+    health_condition_id VARCHAR(50) PRIMARY KEY,
     health_condition_name VARCHAR(50) NOT NULL UNIQUE,
     tags VARCHAR(255) NULL
 );
